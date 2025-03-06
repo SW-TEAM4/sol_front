9ce924e6eec1e6365e0729f0a7c42293054ffb4f
dlrlfgh1210 <dlrlfgh517@gmail.com>
2025-03-06 15:37:46 +0900

    카테고리 모달 정보

---
+++ src/component/category/CompanyNameMapping.js
@@ -0,0 +1,58 @@
+const CompanyNameMapping = {
+
+    // 은행 및 금융권
+    "KakaoBank Corp": "카카오뱅크",
+    "Shinhan Financial Group Co Ltd": "신한지주",
+    "BNK Financial Group Inc": "BNK금융지주",
+    "Hana Financial Group Inc": "하나금융지주",
+    "KB Financial Group Inc": "KB금융",
+    "Industrial Bank of Korea": "기업은행",
+    "DGB Financial Group Co Ltd": "DGB금융지주",
+    "JB Financial Group Co Ltd": "JB금융지주",
+    "Woori Financial Group Inc": "우리금융지주",
+
+    // 바이오
+    "Celltrion Inc": "셀트리온",
+    "Samsung Biologics Co Ltd": "삼성바이오로직스",
+    "Seegene Inc": "씨젠",
+    "GC Cell Corp": "녹십자랩셀",
+
+    // 식품
+    "Samyang Foods Co Ltd": "삼양식품",
+    "Ottogi Corp": "오뚜기",
+    "Orion Corp": "오리온홀딩스",
+    "CJ Cheiljedang Corp": "CJ제일제당",
+
+    // 동영상 플랫폼 및 엔터테인먼트
+    "Hybe Co Ltd": "하이브",
+    "SM Entertainment Co Ltd": "SM엔터테인먼트",
+    "YG Entertainment Inc": "와이지엔터테인먼트",
+
+    // 자동차
+    "Hyundai Motor Co": "현대자동차",
+    "Kia Corp": "기아자동차",
+    "Hyundai Mobis Co Ltd": "현대모비스",
+
+    // 반도체
+    "Samsung Electronics Co Ltd": "삼성전자",
+    "SK Hynix Inc": "SK하이닉스",
+    "Samsung SDI Co Ltd": "삼성SDI",
+    "TESNA Inc": "테스나",
+
+    // 뷰티 및 화장품 관련
+    "Amorepacific Corp": "아모레퍼시픽",
+    "Hankook Cosmetics Manufacturing Co Ltd": "한국화장품제조",
+
+    // 여행 및 항공 관련
+    "Asiana Airlines Inc": "아시아나항공",
+    "Korean Air Lines Co Ltd": "대한항공",
+
+    // 조선업 관련
+    "Samsung Heavy Industries Co Ltd": "삼성중공업",
+    "HD Korea Shipbuilding & Offshr Eng C Ltd": "현대중공업",
+
+    // 기타 필요한 회사명 추가 가능
+};
+
+export default CompanyNameMapping;
+
