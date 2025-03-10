f38f6422c5b800fdd87a587a7f1a808794a51c1f
dlrlfgh1210 <dlrlfgh517@gmail.com>
2025-03-10 18:34:49 +0900

    라우터 추가

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import React from 'react';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import PortfolioList from './component/portfolio/PortfolioList';
+import Header from './common/header/Header';
+import Footer from './common/footer/Footer';
 import Home from './component/home/Home';
+import NewsList from './component/news/NewsList';
 
 function App() {
     return (
         <div className="App">
-            {/*<PortfolioList />*/}
-            <Home />
+            <Router>
+                <Header />
+                <div className="content-container">
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/news" element={<NewsList />} />
+                        <Route
+                            path="/parking"
+                            element={<div>파킹통장 페이지 (준비 중)</div>}
+                        />
+                        <Route
+                            path="/assets"
+                            element={<div>자산 페이지 (준비 중)</div>}
+                        />
+                        <Route
+                            path="/account"
+                            element={<div>내 계좌 페이지 (준비중)</div>}
+                        />
+                    </Routes>
+                </div>
+                <Footer />
+            </Router>
         </div>
     );
 }
